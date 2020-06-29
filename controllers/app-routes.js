684f97d7af271930eca77bad5f1d0a314584508a
neggas <arsene.fop@gmail.com>
2020-06-29 19:08:47 +0200

    dynamisation des contact

--- controllers/app-routes.js
+++ controllers/app-routes.js
@@ -1,4 +1,6 @@
-exports.getIndex = (req,res,next)=>{
-    res.render("index");
+const Users = require('../models/users');
+exports.getIndex = async(req,res,next)=>{
+    const users = await Users.find();
+    res.render("index",{users});
 }
 
--- public/js/dynamiqueActive.js
+++ public/js/dynamiqueActive.js
@@ -1,6 +1,9 @@
 window.onload = ()=>{
     const activeItems = document.querySelectorAll(".contacts .d-flex");
-   
+    const el =  document.querySelector(".contacts");
+    const current = el.firstChild.nextSibling;
+    current.classList.add('active');
+    
     activeItems.forEach((item)=>{
         item.addEventListener("click",(event)=>{
            const activeDiv = item.parentElement;
