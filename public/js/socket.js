87344a330a516211a2eebb453c70b475830cde1a
neggas <arsene.fop@gmail.com>
2020-06-30 13:02:10 +0200

    ajout de de la fonctionnalités system de recherche

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,40 @@
 window.onload = ()=>{
+    let  search = document.querySelector('.input-group .search');
+    let  userInfos = document.querySelectorAll('.ami .user_info');
+
+    //selection dynamique des liste de chat
+    const activeItems = document.querySelectorAll(".contacts .d-flex");
+    const el =  document.querySelector(".contacts");
+    const current = el.firstChild.nextSibling;
+    current.classList.add('active');
+    
+    activeItems.forEach((item)=>{
+        item.addEventListener("click",(event)=>{
+           const activeDiv = item.parentElement;
+           document.querySelector('.contacts .active').classList.remove('active');
+           activeDiv.classList.add('active');
+          
+        })
+    })
+
+    //communication par socket
     socket.on("users",(users)=>{
-        console.log(users);
+
+        search.addEventListener('input',(event)=>{
+            activeItems.forEach((item)=>{
+
+                if(event.target.value !== ''){
+                    if(!(item.children[1].children[0].innerHTML.includes(event.target.value))){
+                        console.log("l'element est cacher");
+                        item.parentElement.style.display = 'none';
+                    }else{
+                        item.parentElement.style.display = 'block';
+                    }
+                }else{
+                    item.parentElement.style.display = 'block';
+                }
+            })
+        })
+
     })
 }
